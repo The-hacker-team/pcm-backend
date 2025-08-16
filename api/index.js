7ef317854bfdea892bad21560e56ba786253b884
Desire <desireirankundawilliams@gmail.com>
2025-08-17 00:02:07 +0200

    Stop tracking .env file

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5174", // or wherever your React app is running
+    origin: "http://localhost:5174",
     credentials: true,
   })
 );
@@ -32,7 +32,6 @@ mongoose
 
   .then(() => {
     console.log("MongoDB connected");
-    console.log("MongoDB connected process", process.env.MONGO_URI);
     app.listen(process.env.PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
