c6164b8aab437d094c6df396694c153fb7ecf1d6
Desire <desireirankundawilliams@gmail.com>
2025-08-17 00:21:54 +0200

    resolved the cor

--- api/index.js
+++ api/index.js
@@ -6,13 +6,29 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5174",
-    credentials: true,
-  })
-);
+
 // Middleware
+app.use(express.json());
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://cavendish-pcm.vercel.app",
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 setupSwagger(app);
 
