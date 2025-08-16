970b797454978e103afa54ae23fbdb15276efca6
Desire <desireirankundawilliams@gmail.com>
2025-08-16 09:15:44 +0200

    cleaned the code

--- models/User.js
+++ models/User.js
@@ -8,8 +8,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: {
     type: String,
-    enum: ["Admin", "Communication", "Secretary", "Treasurer"],
-    default: "Communication",
+    enum: ["admin", "communication", "secretary", "treasurer"],
+    default: "communication",
   },
 });
 
